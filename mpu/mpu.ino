10c1ab85a1d58899b6bc5ef2607932e91b060c96
EPLABORATORY <41285687+EPLABORATORY@users.noreply.github.com>
2018-08-03 18:42:49 +0100

    S-P Code
    Serial Commands Feature + Calibration half done. Code cleaned up and structured. CalibrationCode.ino was added.

--- mpu/mpu.ino
+++ mpu/mpu.ino
@@ -1,3 +1,4 @@
+///Libraries
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
 #include <SPI.h>
@@ -5,40 +6,46 @@
 #include<Wire.h>
 #include <math.h>
 
+//Output Variables
 boolean RecordToSD = true;
+String OutputStr = "";
 
+//MPU Variables
+MPU6050 mpu;
 const int MPU_addr = 0x68; // I2C address of the MPU-6050
 int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ, OldGyZ;
-String str = "";
-String str1 = "";
 
+//Stroke Variables
 long StrokeCounter = 0;
+
 int tempCounter = 0;
 float StrokeRate = 0.0;
 long t;
-
 boolean Strokes[30] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0
                        , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
                        , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
                       };
 
-MPU6050 mpu;
-
-double mapf (double val, double inmin , double inmax , double outmin , double outmax){
-return (val - inmin) * (outmax - outmin) / (inmax - inmin) + outmin;
-}
-
+//Force Variables
 double forceVAL = 0;
 double forceSUM = 0;
 double forceCOUNT = 0;
 double forceAVERAGE = 0;
 
+//Test & Temporary & General Variables
+double AccMag = 0;
+String TestString = "";
 
-void setup() {
 
+
+double mapf (double val, double inmin , double inmax , double outmin , double outmax) {
+  return (val - inmin) * (outmax - outmin) / (inmax - inmin) + outmin;
+}
+
+void InitialiseMPU() {
   Wire.begin();
-
   Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+
   mpu.initialize();
   mpu.dmpInitialize();
   mpu.setXAccelOffset(-1547); // 1688 factory default for my test chip
@@ -54,13 +61,16 @@ void setup() {
   Wire.write(0);     // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
 
-  /* Wire.beginTransmission(MPU_addr);
-    Wire.write(0x1C);
-    Wire.write(B00010000);   //here is the byte for sensitivity (8g here) check datasheet for the one u want
-    Wire.endTransmission(true);
-  */
-  Serial.begin(57600);
 
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x1C);
+  Wire.write(B00010000);   //here is the byte for sensitivity (8g here) check datasheet for the one u want
+  Wire.endTransmission(true);
+
+}
+
+void InitialiseSD() {
+  Serial.begin(57600);
   if (RecordToSD == true) {
     if (!SD.begin(4)) {
       //Serial.println("Card failed, or not present");
@@ -70,19 +80,14 @@ void setup() {
     //Serial.println("Card initialized.");
     File dataFile = SD.open("data.csv", FILE_WRITE);
     if (dataFile) {
-      dataFile.println("Time,X Acc,Y Acc,Z Acc,X Rot,Y Rot,Z Rot,Stroke Counter,AverageForce");
+      dataFile.println("Time,X Acc,Y Acc,Z Acc,X Rot,Y Rot,Z Rot,Stroke Counter,Force Value,Force Average (per stroke)");
       dataFile.close();
-      Serial.println(str);
     }
   }
-  OldGyZ = -3000;
-  t = 0;
-  tempCounter = 0;
 }
-double AccMag = 0;
-String TestString = "";
 
-void ReadMPU(){
+
+void ReadMPU() {
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
@@ -94,99 +99,123 @@ void ReadMPU(){
   GyX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
   GyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-
 }
 
-
-void loop() {
-  ReadMPU();
-  
+void ReadForce() {
   int sensorValue = analogRead(A7);
   double forceVAL = mapf(sensorValue, 0, 1023, 0, 450);
   if (forceVAL > 10) {
     forceSUM += forceVAL;
     forceCOUNT ++;
   }
-  
+}
 
-  int temp = 0;
-  if ((OldGyZ < -2000 && GyZ > 2000 ) || (OldGyZ > 2000 && GyZ < -2000)) {
-    
-    //Each Stroke calculate average force    
-    forceAVERAGE = forceSUM / forceCOUNT;
-    forceSUM = 0;
-    forceCOUNT = 0;
-
-    // Reset Each Stroke
-    OldGyZ = GyZ;
-    StrokeCounter += 1;
-    tempCounter += 1;
-
-    //Temporary Spike so we can see it on the plot...
-    temp = 3000;
-  }
-  
-  
-  if (millis() - t >= 100) {
-    for (int i = 0; i < 29; i++) {
-      Strokes[i] = Strokes[i + 1];
-    }
-
-    //shift.
-    if (tempCounter == 0) {
-      Strokes[29] = false;
-    }
-    else {
-      Strokes[29] = true;
-    }
-    float Counter = 0.0;
-    for (int i = 0; i < 30; i++) {
-      if (Strokes[i] == true) Counter += 1.0;
-    }
-
-    StrokeRate =  (float)Counter * 20 ;
-
-    tempCounter = 0;
-    t = millis();
-  }
-
-
-
-
-  /*
-    if (tempCounter == 4) {
-     StrokeRate = (5 * 60000) / (millis() - t);
-     tempCounter = 0;
-     t = millis();
-    }
-  */
-
-
-
-
-  str =String(millis())+ "," + String(AcX) + ","+ String(AcY) + ","+ String(AcZ) + ","+ String(GyX) + ","+ String(GyY) + ","+ String(GyZ)+ ","+ String(StrokeCounter)+","+String(forceAVERAGE); ;
-  //str = String(AcX) + ","+ String(AcY) + ","+ String(AcZ) + ","+ String(GyX) + ","+ String(GyY) + ","+ String(GyZ);
-  //str = String(AcX) + ","+ String(AcY) + ","+ String(AcZ);
-
-  //str =  String(GyZ) + "," + String(temp) + "," + String(StrokeRate);
-  //str = "Temp Counter" + String(tempCounter) + " Stroke Counter:" + String(StrokeCounter) +", Stroke Rate:"+ String(StrokeRate);
-
+void LogToSerial() {
+  //OutputStr = String(AcX) + ","+ String(AcY) + ","+ String(AcZ) + ","+ String(GyX) + ","+ String(GyY) + ","+ String(GyZ);
+  //OutputStr = String(AcX) + ","+ String(AcY) + ","+ String(AcZ);
+  //OutputStr =  String(GyZ) + "," + String(temp) + "," + String(StrokeRate);
+  //OutputStr = "Temp Counter" + String(tempCounter) + " Stroke Counter:" + String(StrokeCounter) +", Stroke Rate:"+ String(StrokeRate);
   //AccMag = (sqrt((AcX*AcX) + (AcY*AcY) + (AcZ*AcZ)));
 
-  Serial.println(str);
+  OutputStr = String(millis()) + "," + String(AcX) + "," + String(AcY) + "," + String(AcZ) + "," + String(GyX) + "," + String(GyY) + "," + String(GyZ) + "," + String(StrokeCounter) + "," + String(forceVAL) + "," + String(forceAVERAGE);
+  Serial.println(OutputStr);
+}
+
+void LogToSD() {
   if (RecordToSD == true) {
     File dataFile = SD.open("data.csv", FILE_WRITE);
     if (dataFile) {
-      dataFile.println(str);
+      dataFile.println(OutputStr);
       dataFile.close();
     }
   }
 }
 
-//16bit ADC means output is 0 to 65536
-//ie.  + or - 32768 representing 16G each way
-// g = 9.80665m/s2 therefore 16 * G = 156.9064m/s2
-// 156.9064 / 32768 = 0.0047884033203125 m/s2
-// 1 slice of the ADC is 0.0047884033203125 m/s2
+void EventHandler_Every_100ms() {
+  for (int i = 0; i < 29; i++) {
+    Strokes[i] = Strokes[i + 1];
+  }
 
+  //shift.
+  if (tempCounter == 0) {
+    Strokes[29] = false;
+  }
+  else {
+    Strokes[29] = true;
+  }
+  float Counter = 0.0;
+  for (int i = 0; i < 30; i++) {
+    if (Strokes[i] == true) Counter += 1.0;
+  }
+
+  StrokeRate =  (float)Counter * 20 ;
+
+  tempCounter = 0;
+}
+
+void EventHandler_Every_1000ms() {
+  //No Code yet...
+}
+
+void EventHandler_Every_Stroke() {
+  //Each Stroke calculate average force
+  forceAVERAGE = forceSUM / forceCOUNT;
+  forceSUM = 0;
+  forceCOUNT = 0;
+
+  // Reset Each Stroke
+  OldGyZ = GyZ;
+  StrokeCounter += 1;
+  tempCounter += 1;
+}
+
+void ProcessStrokes() {
+  if ((OldGyZ < -2000 && GyZ > 2000 ) || (OldGyZ > 2000 && GyZ < -2000)) {
+    EventHandler_Every_Stroke();
+  }
+}
+
+void ProcessTimers() {
+  if (millis() - t >= 100) {
+    EventHandler_Every_100ms();
+  }
+  if (millis() - t >= 1000) {
+    EventHandler_Every_1000ms();
+  }
+  t = millis();
+}
+
+void setup() {
+  InitialiseMPU();
+  InitialiseSD();
+  OldGyZ = -3000;
+  t = 0;
+  tempCounter = 0;
+}
+
+void ReadSerialCommands() {
+  while (Serial.available()) {
+    String Command;
+    Command = Serial.readString();// read the incoming data as string
+    if (Command == "calibrate") {
+      CalibrateMPU();
+    }
+    if (Command == "on") {
+      digitalWrite(13,HIGH); 
+    }
+    if (Command == "off") {
+      digitalWrite(13,HIGH); 
+    }    
+  }
+}
+
+void loop() {
+  ReadMPU();
+  ReadForce();
+  ReadSerialCommands();
+  ProcessTimers();
+  ProcessStrokes();
+  LogToSD();
+  LogToSerial();
+}
 
